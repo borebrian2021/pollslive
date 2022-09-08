bc1aea4fa6b7275dfa1b022560c30da8ebf6d6cc
mnjoki21 <mitchellenjoki7@gmail.com>
2022-09-08 10:22:21 +0300

    adding get request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,13 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project Mitchelle!" }.to_json
   end
 
-  get "/books" do
-    
-    
+  get '/books' do
+    books = Book.all
+    books.to_json 
+  end
+
+  get '/books/:id' do
+    books = Books.find(params[:id])
   end
 
 end
