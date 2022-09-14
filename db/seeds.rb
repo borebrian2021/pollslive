67731c37be84c4b435d3f76616cd319624b8f624
borebrian2021 <bkimutai2021@gmail.com>
2022-09-14 09:39:39 +0300

    Modified seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@ puts "🌱 Seedie seeding seeds..."
     party_name: Faker::Book.title,
     candidate_name: Faker::Book.author,
     profile_image: Faker::Book.genre,
-    string: Faker::Book.publisher   
+    party_logo: Faker::Book.publisher   
     )
 end
+
 
 
 
