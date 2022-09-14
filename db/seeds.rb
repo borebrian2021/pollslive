a877c41c7c21e83bbb8fcfbe4a87c30c0874a40a
borebrian2021 <bkimutai2021@gmail.com>
2022-09-14 08:53:52 +0300

    Polished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,14 @@
 puts "🌱 Seedie seeding seeds..."
 
 10.times do
-    Book.create(
-     title: Faker::Book.title,
-     author: Faker::Book.author,
-     genre: Faker::Book.genre,
-     publisher: Faker::Book.publisher   
+    Party.create(
+    party_name: Faker::Book.title,
+    candidate_name: Faker::Book.author,
+    profile_image: Faker::Book.genre,
+    string: Faker::Book.publisher   
     )
 end
 
-10.times do 
-    Purchase.create(
-        purchase_title: Faker::Book.title,
-        customer_id: rand(1..20),
-        book_id: rand(1..20)
-    )
-end
-
-10.times do
-    Genre.create(
-        fiction: Faker::Book.genre,
-        romance: Faker::Book.genre,
-        horror: Faker::Book.genre,
-        sci_fi: Faker::Book.genre
-    )
-end
-
-10.times do
-    Customers.create(
-        name: Faker::Name.unique.name_with_middle
-    )
-end
 
 
 puts "✅ Done seeding!"
